e794e8cd3ac6e416a0ccdfe64cdffc3e92ad6ada
Aida Radfard <aida.radfard.1@gmail.com>
2022-11-12 21:44:48 +0330

    handle onClick and key press and save city name in a state

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,25 +1,37 @@
-import {React , useState , useRef , useEffect} from 'react'
+import {React , useRef , useEffect, useState} from 'react'
 import './search.css';
 import SearchIcon from '@mui/icons-material/Search';
 
 export const Search = () => {
 
     const searchInput = useRef();
+    const [searchedCity , setSearchedCity] = useState([]);
 
     useEffect(() => {
       searchInput.current.focus();
     }, [])
     
 
-    const handleClick = () => {
-      console.log(searchInput.current.value)
+    const handleClick = (city) => {
+      setSearchedCity(city)
+      searchInput.current.value = '' ;
     }
 
-
+    const handleKeyDown = (e) => {
+      if(e.key === 'Enter' ){
+        handleClick(searchInput.current.value);
+      }
+    }
+    
+console.log(searchedCity)
   return (
     <div className='search-box'>
-        <input type={'text'} ref={searchInput} className="search-input" />
-        <button onClick={() => handleClick()} className='search-btn'><SearchIcon className='search-icon' /></button>
+        <input type={'text'} ref={searchInput} className="search-input" onKeyDown={(e) => handleKeyDown(e)} />
+        <button 
+          onClick={() => handleClick(searchInput.current.value)} 
+          className='search-btn'>
+            <SearchIcon className='search-icon' />
+        </button>
     </div>
   )
 }
